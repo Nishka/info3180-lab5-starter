57e31bddda4cefc07a75f9af931207b03c1dce62
taniska <taniskan@yahoo.com>
2017-03-12 00:16:34 +0000

    first commit

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, IntegerField,SelectField,FileField
 from wtforms.validators import InputRequired
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+    
+class dataform(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    firstname = StringField('Firstname', validators=[InputRequired()])
+    lastname = StringField('Lastname', validators=[InputRequired()])
+    age = IntegerField("age",validators=[InputRequired()])
+    biography= StringField('biography',validators= [InputRequired()])
+    gender =  SelectField('gender', choices =[('female','female'),('male','male')],validators=[InputRequired()])
+    image = FileField("file", validators=[InputRequired()])
